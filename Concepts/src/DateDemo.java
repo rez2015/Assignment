a8b53b95d2049a738af3d8dae3323b55fdc8ebec
rez2015 <sultanarezw@gmail.com>
2015-05-13 00:49:45 -0400

    git remote add origin https://github.com/rez2015/Assignment.git

---
+++ Concepts/src/DateDemo.java
@@ -0,0 +1,19 @@
+/**
+ * Created by smasumon on 5/12/2015.
+ */
+
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[]arg){
+
+
+        Date date = new Date();
+
+        System.out.println(date.toString());
+    }
+
+
+
+
+}
---
+++ Concepts/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by smasumon on 5/12/2015.
+ */
+public class HelloWorld {
+
+public static void main (String[]arg){
+    System.out.println("Hello World");
+
+}
+
+}
